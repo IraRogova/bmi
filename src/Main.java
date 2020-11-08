0189ac6d1903b61221e6b7da6470e1e87c75f3a5
Irina Rogova <buyersbirds@gmail.com>
2020-11-08 13:15:17 +0600

    change main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bodyMassIndex = service.calculate(75,170);
+        double bodyMassIndex = service.calculate(64,174);
         System.out.println(bodyMassIndex);
     }
 }
