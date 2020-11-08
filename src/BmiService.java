3d68231c93fe8588a7b871a59897b165c472ce51
Irina Rogova <buyersbirds@gmail.com>
2020-11-08 18:35:56 +0600

    Changed both files

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,6 @@
 public class BmiService {
     public double calculate(double w, double h) {
-        w = 64;
-        h = 1.74;
-        double bodyMassIndex;
-        bodyMassIndex = w / (h * h);
+        double bodyMassIndex = w / (h * h);
         return bodyMassIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bodyMassIndex = service.calculate(64,174);
+        double w = 64;
+        double h = 1.74;
+        double bodyMassIndex = service.calculate(w,h);
         System.out.println(bodyMassIndex);
     }
 }
